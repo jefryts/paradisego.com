e5daa2bdecfd157a46f0dfe94c22019d373e6d69
jefryts <36080604+jefryts@users.noreply.github.com>
2018-05-23 23:54:02 -0500

    Initial commit

---
+++ resources/assets/js/App.vue
@@ -0,0 +1,10 @@
+<template>
+	<div class="container">
+		<div class="navbar">&nbsp;</div>
+		<div class="content">
+			<transition name="fade" mode="out-in">
+				<router-view></router-view>
+			</transition>
+		</div>
+	</div>
+</template>
---
+++ resources/assets/js/views/events/index.vue
@@ -0,0 +1,63 @@
+<template>
+	<div>
+		<p>HOLA</p>
+	</div>
+</template>
+<script type="text/javascript">
+	import Vue from 'vue'
+	// import { get } from '../../lib/api'
+	export default {
+		data () {
+			return {
+				model : {
+					data: []
+				}
+			}
+		},
+	// 	beforeRouteEnter(to, from, next) {
+	// 		get('/api/invoices', to.query)
+	// 			.then((res) => {
+	// 				next(vm => vm.setData(res))
+	// 			})
+	// 	},
+	// 	beforeRouteUpdate(to, from, next) {
+	// 		get('/api/invoices', to.query)
+	// 			.then((res) => {
+	// 				this.setData(res)
+	// 				next()
+	// 			})
+	// 	},
+	// 	methods: {
+	// 		setData(res) {
+	// 			Vue.set(this.$data, 'model', res.data.results)
+	// 			this.page = this.model.current_page
+	// 			this.$bar.finish()
+	// 		},
+	// 		nextPage() {
+	// 			if(this.model.next_page_url) {
+	// 				const query = Object.assign({}, this.$route.query)
+	// 				query.page = query.page ? (Number(query.page) + 1) : 2
+
+	// 				this.$router.push({
+	// 					path: '/invoices',
+	// 					query: query
+	// 				})
+	// 			}
+	// 		},
+	// 		prevPage() {
+	// 			if(this.model.prev_page_url) {
+	// 				const query = Object.assign({}, this.$route.query)
+	// 				query.page = query.page ? (Number(query.page) - 1) : 1
+
+	// 				this.$router.push({
+	// 					path: '/invoices',
+	// 					query: query
+	// 				})
+	// 			}
+	// 		},
+	// 		detailsPage(item) {
+	// 			this.$router.push(`/invoices/${item.id}`)
+	// 		}
+	// 	}
+	}
+</script>
